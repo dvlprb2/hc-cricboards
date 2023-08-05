94b0f526acc34906ff426ec44cfa5c8e63640feb
Bittu Patel <hello@developerb2.com>
2023-08-04 21:58:27 -0400

    Fixing Git Repo

--- Readme.md
+++
@@ -1,1 +0,0 @@
-# HexClan Cricboards
--- components/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import {Icon, Input, VStack} from "native-base";
-import {MaterialIcons} from "@expo/vector-icons";
-
-export const SearchBar = () => {
-    return <VStack w="98%" space={5} alignSelf="center">
-        <Input placeholder="Search Player" size="2xl"
-               InputLeftElement={<Icon m="2" ml="3" size="6" as={<MaterialIcons name="search"/>}/>}/>
-    </VStack>;
-}
