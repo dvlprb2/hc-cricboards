a69fb3f5e759116bb3636c9d944769435001cbf3
Akshil <shahakshil21@gmail.com>
2023-08-05 00:33:41 -0400

    mock the data for member's flaglist

--- src/screens/club/Members.tsx
+++ src/screens/club/Members.tsx
@@ -49,11 +49,68 @@ export const MembersTab: React.FC<{navigation: any}> = ({navigation}) => {
     );
   };
 
+  const data: MemberData[] = [
+    {
+      id: '1',
+      name: 'Bittu Patel',
+      joinDate: 'June 16, 2023',
+      matches: '11 MATCHES',
+    },
+    {
+      id: '2',
+      name: 'Smit',
+      joinDate: 'May 25, 2023',
+      matches: '25 MATCHES',
+    },
+    {
+      id: '3',
+      name: 'Rahul Shah',
+      joinDate: 'May 02, 2023',
+      matches: '2 MATCHES',
+    },
+    {
+      id: '4',
+      name: 'Dhruv Jani',
+      joinDate: 'February 02, 2023',
+      matches: '9 MATCHES',
+    },
+    {
+      id: '5',
+      name: 'Sachin Patel',
+      joinDate: 'March 31, 2023',
+      matches: '18 MATCHES',
+    },
+    {
+      id: '6',
+      name: 'Jigar Patel',
+      joinDate: 'January 15, 2023',
+      matches: '45 MATCHES',
+    },
+    {
+      id: '7',
+      name: 'Soury Patel',
+      joinDate: 'March 12, 2023',
+      matches: '26 MATCHES',
+    },
+    {
+      id: '8',
+      name: 'Montu Patel',
+      joinDate: 'May 09, 2023',
+      matches: '5 MATCHES',
+    },
+    {
+      id: '9',
+      name: 'Ronak Soni',
+      joinDate: 'June 02, 2023',
+      matches: '7 MATCHES',
+    },
+  ];
+
   return (
     <>
       <FlatList
         keyExtractor={item => item.id}
-        //datd = 
+        data = {data}
         ListHeaderComponent={<SearchBar my={3} />}
         ListEmptyComponent={<EmptyList />}
         renderItem={renderItem}
